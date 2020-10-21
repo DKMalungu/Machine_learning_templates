71b5bafe7b38776387b876e3dc6b3e7879b45bbf
DKMalungu <danielkmalungu@gmail.com>
2020-10-21 09:46:24 +0300

    'frequency plot update for frequency polygon'

--- statistics/graphically_quantitative_data.py
+++ statistics/graphically_quantitative_data.py
@@ -23,3 +23,80 @@ plt.show()
 plt.hist(x=dataset, bins=7)
 plt.title('matplotlib implementation')
 plt.show()
+
+# Plotting frequency plot data in defines classes (The test involves class scores)
+marks = [32,
+         45,
+         50,
+         56,
+         58,
+         60,
+         61,
+         61,
+         63,
+         64,
+         64,
+         65,
+         66,
+         67,
+         67,
+         68,
+         69,
+         69,
+         72,
+         76,
+         78,
+         81,
+         83,
+         83,
+         85,
+         86,
+         87,
+         87,
+         88,
+         89,
+         90,
+         90,
+         91,
+         92,
+         92,
+         93,
+         93,
+         94,
+         96,
+         98]
+
+# Creating the classes
+g = 0
+a = 0
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
+
+for i in marks:
+    print(i)
+    if i < 40:
+        print(i)
+        g += 1
+        print(g)
+    elif i < 50:
+        f += 1
+    elif i < 60:
+        e += 1
+    elif i < 70:
+        d += 1
+    elif i < 80:
+        c += 1
+    elif i < 90:
+        b += 1
+    else:
+        a = +1
+data_class = {'scores_frequency': [a, b, c, d, e, f, g],
+              'score_classes': ['90<100', '80<90', '70<80', '60<70', '50<60', '40<50', '30<40']}
+
+marks_data = pd.DataFrame(data=data_class)
+print(marks_data)
+marks_data.plot.bar(x='score_classes', y='scores_frequency')
+plt.show()
